1565cf095eb8a984161fe93380beeb25719772e4
bfank9@gmail.com <bfank9@gmail.com>
2021-02-17 23:24:39 -0700

    books comments

--- Models/Books.cs
+++ Models/Books.cs
@@ -8,12 +8,14 @@ namespace IS_413_Assignment_5.Models
 {
     public class Books
     {
+        //primary key
         [Key]
         public int BookId { get; set; }
 
         [Required]
         public string Title { get; set; }
 
+        //no non atomic fields
         [Required]
         public string AuthorFirst { get; set; }
 
@@ -24,16 +26,19 @@ namespace IS_413_Assignment_5.Models
         [Required]
         public string Publisher { get; set; }
 
+        //regular expression validation
         [Required(ErrorMessage ="ISBN must be in ###-########## format")]
         [RegularExpression(@"([0-9]{3})[-]([0-9]{10}$")]
         public string ISBN { get; set; }
 
+        //split up the filed category and classification
         [Required]
         public string Category { get; set; }
 
         [Required]
         public string Classification { get; set; }
 
+        //make this a currency
         [Required]
         [DataType(DataType.Currency)]
         public double Price { get; set; }
