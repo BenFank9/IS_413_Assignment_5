6267611d3da67f4e9781277a602a9f775fe640e8
bfank9@gmail.com <bfank9@gmail.com>
2021-03-10 16:08:12 -0700

    functioning cart

--- Pages/Buy.cshtml.cs
+++ Pages/Buy.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IS_413_Assignment_5.Infrastructure;
+using IS_413_Assignment_5.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,35 @@ namespace IS_413_Assignment_5.Pages
 {
     public class BuyModel : PageModel
     {
-        public void OnGet()
+        private IBookstoreRepository repository;
+
+        //constructor
+        public BuyModel(IBookstoreRepository repo)
         {
+            repository = repo;
+        }
+
+        //build the cart
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+        //methods
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long bookId, string returnUrl)
+        {
+            Books books = repository.Books.FirstOrDefault(p => p.BookId == bookId);
+
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+
+            Cart.AddItem(books, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
